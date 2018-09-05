ba474c8c46f5eb55e90ecc0b6a913ab5bb2123b6
Aaron Frank <aaron.frank41@gmail.com>
2018-09-05 11:37:45 -0700

    solved challenge 6

--- forEach/challenges-01.test.js
+++ forEach/challenges-01.test.js
@@ -121,12 +121,18 @@ const addNumbers = (num, arr, times, callback) => {
 // ------------------------------------------------------------------------------------------------
 
 const removeOne = (num, input) => {
-  // Solution code here...
-}
+  if(num % 3 === 2) {
+    input.pop();
+  };
+};
 
 const removeElements = (input, callback) => {
-  // Solution code here...
-}
+  for (let item of input) {
+    callback(item, input);
+  };
+
+  return input;
+};
 
 // ------------------------------------------------------------------------------------------------
 // CHALLENGE 7
